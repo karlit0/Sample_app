d8c4216fc17f638c29a9ca1882fd8bfd74145088
Karlit0 <antonio.stipanovic@fer.hr>
2013-02-12 23:50:34 +0100

    Pretty spec code - use shared examples & let

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,16 @@ describe "User pages" do
 
 	subject { page }
 	
+	shared_examples_for "all user pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_selector('title', text: full_title(page_title)) }
+	end
+
 	describe "Signup page" do
 		before { visit signup_path }	
+		let (:heading) { 'Sign Up' }
+		let (:page_title) { 'Sign up' }
 
-		it { should have_selector('h1', text: 'Sign Up') }
-		it { should have_selector('title', text: full_title('Sign up')) }  
+		it_should_behave_like "all user pages"
 	end
 end
